13adc19893ceb007dba1cf816a98d907e8a85f8c
Philipp Brummer <philipp.maximilian.brummer@cern.ch>
2016-08-12 16:19:38 +0200

    Implemented shallow compare utility function in order to compare snapshot elements by primitive values
    Further (deep) comparison might be necessary in order to determine whether or not a component should update.

--- dist/js/utilities/daqview-util.js
+++ dist/js/utilities/daqview-util.js
@@ -8,4 +8,23 @@ var DAQViewUtility;
         }
     }
     DAQViewUtility.forEachOwnObjectProperty = forEachOwnObjectProperty;
+    function isPrimitiveType(type) {
+        return (type === 'string' || type === 'number' || type === 'boolean');
+    }
+    function areEqualShallow(a, b) {
+        if (a['@id'] !== b['@id'])
+            return false;
+        for (var key in a) {
+            if (a.hasOwnProperty(key)) {
+                var value = a[key];
+                var valueType = typeof (value);
+                if (value === null || isPrimitiveType(valueType)) {
+                    if (value !== b[key])
+                        return false;
+                }
+            }
+        }
+        return true;
+    }
+    DAQViewUtility.areEqualShallow = areEqualShallow;
 })(DAQViewUtility || (DAQViewUtility = {}));
--- src/js/daqview-react/utilities/daqview-util.ts
+++ src/js/daqview-react/utilities/daqview-util.ts
@@ -8,4 +8,24 @@ namespace DAQViewUtility {
         }
     }
 
+    function isPrimitiveType(type: string) {
+        return (type === 'string' || type === 'number' || type === 'boolean');
+    }
+
+    export function areEqualShallow(a: DAQAggregator.Snapshot.SnapshotElement, b: DAQAggregator.Snapshot.SnapshotElement) {
+        if (a['@id'] !== b['@id']) return false;
+
+        for (let key in a) {
+            if (a.hasOwnProperty(key)) {
+                let value: any = a[key];
+                let valueType: string = typeof(value);
+                if (value === null || isPrimitiveType(valueType)) {
+                    if (value !== b[key]) return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
 }
