b69733889f66e9f227b558922960aa70be3a282f
Michail Vougioukas <michail.vougioukas@cern.ch>
2017-02-06 15:29:11 +0100

    fix(): backwards compatibility for snapshots without tcdsglobalinfo object

--- src/js/daqview-react/structures/daq-aggregator/daq-snapshot.ts
+++ src/js/daqview-react/structures/daq-aggregator/daq-snapshot.ts
@@ -54,7 +54,7 @@ namespace DAQAggregator {
             fedBuilderSummary: FEDBuilderSummary;
             buSummary: BUSummary;
 
-            tcdsGlobalInfo: TCDSGlobalInfo;
+            tcdsGlobalInfo?: TCDSGlobalInfo;
 
         }
 
