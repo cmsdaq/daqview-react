22fe1757837a0616e8e63f6ba4d45af76c9a9a09
Philipp Brummer <philipp2812@gmail.com>
2017-08-04 16:10:28 +0200

    chore: update transpiled JavaScript

--- dist/js/components/common/snapshot-modal.js
+++ dist/js/components/common/snapshot-modal.js
@@ -1,11 +1,17 @@
+"use strict";
 /**
  * Created by mvougiou on 1/11/17.
  */
-var __extends = (this && this.__extends) || function (d, b) {
-    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
-    function __() { this.constructor = d; }
-    d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
-};
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = Object.setPrototypeOf ||
+        ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+        function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 /**
  * @author Michail Vougioukas
  * @author Philipp Brummer
@@ -24,12 +30,12 @@ var DAQView;
             this.url = url;
             if (!snapshot) {
                 var msg = "";
-                var errRootElement = React.createElement(ErrorElement, {message: msg});
+                var errRootElement = React.createElement(ErrorElement, { message: msg });
                 ReactDOM.render(errRootElement, this.htmlRootElement);
             }
             else {
                 var daq = snapshot.getDAQ();
-                var snapshotModalRootElement = React.createElement(SnapshotModalElement, {daq: daq, url: url});
+                var snapshotModalRootElement = React.createElement(SnapshotModalElement, { daq: daq, url: url });
                 ReactDOM.render(snapshotModalRootElement, this.htmlRootElement);
             }
         };
@@ -42,17 +48,20 @@ var DAQView;
     var SnapshotModalElement = (function (_super) {
         __extends(SnapshotModalElement, _super);
         function SnapshotModalElement() {
-            _super.apply(this, arguments);
+            return _super !== null && _super.apply(this, arguments) || this;
         }
         SnapshotModalElement.prototype.render = function () {
-            return (React.createElement("div", null, React.createElement("button", {className: "button-share"}, "Share"), React.createElement("a", {href: this.props.url, target: "_blank"}, React.createElement("button", {className: "button-snapshot"}, "See raw DAQ snapshot"))));
+            return (React.createElement("div", null,
+                React.createElement("button", { className: "button-share" }, "Share"),
+                React.createElement("a", { href: this.props.url, target: "_blank" },
+                    React.createElement("button", { className: "button-snapshot" }, "See raw DAQ snapshot"))));
         };
         return SnapshotModalElement;
     }(React.Component));
     var ErrorElement = (function (_super) {
         __extends(ErrorElement, _super);
         function ErrorElement() {
-            _super.apply(this, arguments);
+            return _super !== null && _super.apply(this, arguments) || this;
         }
         ErrorElement.prototype.render = function () {
             return (React.createElement("div", null, this.props.message));
