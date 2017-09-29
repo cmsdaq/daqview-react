f115eab697335c075e084ce2bb77dcb055c9c7c2
Philipp Maximilian Brummer <philipp.maximilian.brummer@cern.ch>
2017-09-29 18:00:02 +0200

    feat: add page displaying the deadtime table along with the fbs

--- configuration/link-configuration.pro.js
+++ configuration/link-configuration.pro.js
@@ -6,6 +6,7 @@ var daqviewLinks = {
     "base_all": "http://daq-expert.cms/daq2view-react/index.html",
     "base_fb": "http://daq-expert.cms/daq2view-react/index_fb.html",
     "base_fff": "http://daq-expert.cms/daq2view-react/index_fff.html",
+    "base_fbdt": "http://daq-expert.cms/daq2view-react/index_fb_dt.html",
     "controller": "http://daq-expert.cms:8081/DAQSnapshotService/daqviews.jsp"
 };
 
